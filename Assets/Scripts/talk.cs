7c12a5891fa62a72332e4024f128cb10658518d4
Honda <phelzwilling@gmail.com>
2025-11-18 12:12:25 +0900

    add textfont

---
+++ Assets/Scripts/talk.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using static UnityEngine.SceneManagement.SceneManager;
+
+public class talk : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+}
