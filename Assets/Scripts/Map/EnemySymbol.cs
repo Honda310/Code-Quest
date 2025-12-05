c218b3ec90bc30a92d6249840477b42e0cd8f3d4
34007 <34007@iwaki-cc.jp>
2025-12-05 17:03:55 +0900

    no message

---
+++ Assets/Scripts/Map/EnemySymbol.cs
@@ -0,0 +1,34 @@
+// ==================================================
+// File: C:\Users\34007\CodeQuest\Assets\Scripts\Map\EnemySymbol.cs
+// ==================================================
+using UnityEngine;
+
+/// <summary>
+/// 【敵シンボルクラス】
+/// マップ上に配置する敵オブジェクトにアタッチします。
+/// プレイヤーとの接触でバトルを開始します。
+/// </summary>
+public class EnemySymbol : MonoBehaviour
+{
+    // CSVに登録されている敵ID
+    public int EnemyID = 1;
+
+    private bool isDefeated = false;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("colid");
+        // プレイヤーと接触し、まだ倒されていない場合
+        if (col.gameObject.CompareTag("Player") && !isDefeated)
+        {
+            Debug.Log("battle");
+            // バトル開始
+            GameManager.Instance.GetComponent<BattleManager>()
+                .StartBattle(GameManager.Instance.player, GameManager.Instance.neto, EnemyID);
+
+            // 倒されたことにする (バトル終了処理で設定を戻すか、オブジェクトを非表示にする)
+            // isDefeated = true; 
+            gameObject.SetActive(false); // 一旦シンボルを非表示
+        }
+    }
+}
