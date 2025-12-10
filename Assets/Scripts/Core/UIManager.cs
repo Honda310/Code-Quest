48db58cf90824c488b31510b2525521111105b3d
DESKTOP-47GBOL9\yuuki <phelzwilling@gmail.com>
2025-12-11 01:38:15 +0900

    no message

--- Assets/Scripts/Core/UIManager.cs
+++ Assets/Scripts/Core/UIManager.cs
@@ -187,7 +187,7 @@ public class UIManager : MonoBehaviour
     public void OnNetoSearchButtonClicked(Button clickedButton)
     {
         NetoselectPanel.SetActive(false);
-        HealthDpSlidersAndCharactersPanel.SetActive(true);
+        HealthDpSlidersAndCharactersPanel.SetActive(false);
         QuestFramePanel.SetActive(true);
     }
     public void OnNetoItemButtonClicked(Button clickedButton)
