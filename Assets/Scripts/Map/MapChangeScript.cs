4ada6d3a83c7693fa695d84444b9f8a8f018155a
sus160 <phelzwilling@gmail.com>
2026-01-09 11:42:15 +0900

    MoveCheck

---
+++ Assets/Scripts/Map/MapChangeScript.cs
@@ -0,0 +1,28 @@
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class MapChangeScript : MonoBehaviour
+{
+    [SerializeField] private int TransId;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") == false) return;
+        switch (TransId)
+        {
+            case 0:
+                GameManager.Instance.mapManager.TransAnotherMap("ToNeto", TransId);
+                break;
+            case 1:
+                GameManager.Instance.mapManager.TransAnotherMap("InFrontOfLamentForest", TransId);
+                break;
+            case 2:
+                GameManager.Instance.mapManager.TransAnotherMap("LamentForest", TransId);
+                break;
+            case 3:
+                GameManager.Instance.mapManager.TransAnotherMap("PoisonedSpring", TransId);
+                break;
+        }   
+        
+    }
+}
--- Assets/Scripts/Playable/SpawnPlayer.cs
+++ Assets/Scripts/Playable/SpawnPlayer.cs
@@ -7,12 +7,20 @@ public class SpawnPlayer : MonoBehaviour
         switch (SpawnID)
         {
             case 0:
-                GameManager.Instance.player.transform.position = new Vector3(90,50,0);
-                GameManager.Instance.neto.transform.position = new Vector3(60,50,0);
+                GameManager.Instance.player.transform.position = new Vector3(-110,30,0);
+                GameManager.Instance.neto.transform.position = new Vector3(-140,50,0);
                 break;
             case 1:
-                GameManager.Instance.player.transform.position = new Vector3();
-                GameManager.Instance.neto.transform.position = new Vector3();
+                GameManager.Instance.player.transform.position = new Vector3(-100,60,0);
+                GameManager.Instance.neto.transform.position = new Vector3(-130,60,0);
+                break;
+            case 2:
+                GameManager.Instance.player.transform.position = new Vector3(90, 50, 0);
+                GameManager.Instance.neto.transform.position = new Vector3(60, 50, 0);
+                break;
+            case 3:
+                GameManager.Instance.player.transform.position = new Vector3(180,50,0);
+                GameManager.Instance.neto.transform.position = new Vector3(150,50,0);
                 break;
         }
     }
